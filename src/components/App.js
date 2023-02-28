c4b15a2fce0668cbfc9720026dce8f9e3fd3cd9c
Nambardar212 <nikhilsaini0210@gmail.com>
2023-02-28 14:16:45 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,52 @@ const App = () => {
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
+  
+  const handleStart = () => {
+        startTime.current = setInterval(() => {
+          setCurrentTime((prevTime) => prevTime + .010)
+        }, 10)
+  }
+
+  const handleStop = () => {
+       clearInterval(startTime.current);
+  }
+
+  const handleReset = () => {
+     clearInterval(startTime.current);
+     setCurrentTime(0);
+     setLaps([]);
+     intervalRef.current = 0;
+  }
+   
+  const handleLap = () => {
+         setLaps([...laps, currentTime]);
+         intervalRef.current = 1;
+  }
 
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{currentTime.toFixed(3)}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={handleStart}>START</button>
+          <button className="stop-btn" onClick={handleStop}>STOP</button>
+          <button className="lap-btn" onClick={handleLap}>LAP</button>
+          <button className="reset-btn" onClick={handleReset}>RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
+      {
+        intervalRef.current === 1 && <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+          {
+            laps.map((lap, idx) => (
+              <p key={idx}>{lap.toFixed(3)}</p>
+            ))
+          }
         </section>
       </section>
+      }
     </div>
   )
 }
